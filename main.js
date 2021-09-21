0728d3721d6e5bf5d5c9d4ceaf25216c88966538
Zakaria <zakariaharrane2@gmail.com>
2021-09-21 13:51:54 -0700

    Types

---
+++ main.js
@@ -0,0 +1,16 @@
+var a;
+var b;
+var c;
+var d;
+var e = [10, 20, 30];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var backGroundColor = Color.Blue;
---
+++ main.ts
@@ -0,0 +1,14 @@
+let a : number;
+let b : boolean;
+let c : string;
+let d : any;
+let e : number[] = [10,20,30];
+let f : any[] = [1,true,'a', false]
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color {Red = 0 , Green = 1 , Blue = 2}
+
+let backGroundColor = Color.Blue
