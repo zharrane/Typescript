fb22c572145ff280ccbac9fc9d2623e176671480
Zakaria <zakariaharrane2@gmail.com>
2021-09-21 14:26:29 -0700

    Classes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Types
 var a;
 var b;
 var c;
@@ -14,3 +15,42 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 var backGroundColor = Color.Blue;
+// type assertion
+var message;
+message = 'abc';
+// let endWithC =(<string> message).endsWith('c')
+// let endWithCAnotherApproach =(message as string).endsWith('c')
+// functions and arrow functions
+var log = function (msg) {
+    console.log(msg);
+};
+var arrowLog = function (msg) { return console.log(msg); };
+// Custom types
+// interface Point {
+//     x:number,
+//     y:number, 
+//     drawPoint : ()=>void,
+//     getDistance : ()=>void,
+// }
+//let drawPoint  = (point:Point) =>{
+//     //....
+// }
+// let getDistance = (A:Point , B: Point) =>{
+//     //....
+// }
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.drawPoint = function () {
+        //.....
+        console.log("X: " + this.x + " , Y:" + this.y);
+    };
+    Point.prototype.getDistance = function (point) {
+        //.....
+    };
+    return Point;
+}());
+var point = new Point();
+point.x = 10;
+point.y = 20;
+point.drawPoint();
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+//Types
+
 let a : number;
 let b : boolean;
 let c : string;
@@ -12,3 +14,54 @@ const ColorBlue = 2;
 enum Color {Red = 0 , Green = 1 , Blue = 2}
 
 let backGroundColor = Color.Blue
+
+// type assertion
+let message ;
+message='abc'
+// let endWithC =(<string> message).endsWith('c')
+// let endWithCAnotherApproach =(message as string).endsWith('c')
+
+// functions and arrow functions
+
+let log = function (msg) {
+    console.log (msg)
+}
+
+let arrowLog = (msg)=> console.log(msg)
+
+// Custom types
+
+// interface Point {
+//     x:number,
+//     y:number, 
+//     drawPoint : ()=>void,
+//     getDistance : ()=>void,
+// }
+
+
+    //let drawPoint  = (point:Point) =>{
+    //     //....
+    // }
+    // let getDistance = (A:Point , B: Point) =>{
+    //     //....
+    // }
+    
+    
+
+class Point  {
+    x:number ;
+    y: number ;
+    drawPoint () {
+        //.....
+        console.log(`X: ${this.x} , Y:${this.y}`)
+    }
+    getDistance( point:Point) {
+        //.....
+    }
+}
+
+
+let point = new Point();
+point.x = 10
+point.y = 20
+point.drawPoint()
